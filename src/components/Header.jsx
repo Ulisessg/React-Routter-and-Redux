aaa6b789daed18960f7d953629f12f559b3c7981
Juan Corredor <rexejej@gmail.com>
2020-08-12 11:40:19 -0500

    estado final

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,67 @@
-import React from 'react';
-import '../assets/styles/components/Header.scss';
-import logo from '../assets/static/logo-platzi-video-BW2.png';
-import userIcon from '../assets/static/user-icon.png';
+import React from "react";
+import { connect } from 'react-redux';
+import { Link } from "react-router-dom";
+import classNames from 'classnames';
+import gravatar from '../utils/gravatar';
+import { logoutRequest } from '../actions';
+import "../assets/styles/components/Header.scss";
+import logo from "../assets/static/logo-platzi-video-BW2.png";
+import userIcon from "../assets/static/user-icon.png";
+import { object } from "prop-types";
 
-const Header = () => (
-  <header className="header">
-    <img className="header__img" src={logo} alt="Platzi Video" />
-    <div className="header__menu">
-      <div className="header__menu--profile">
-        <img src={userIcon} alt="" />
-        <p>Perfil</p>
+const Header = props => {
+  const { user, isLogin, isRegister } = props;
+  const hasUser = Object.keys(user).length > 0;
+
+
+  const handleLogout = () => {
+    props.logoutRequest({})
+  }
+  const headerClass = classNames('header', {
+    isLogin,
+    isRegister,
+  });
+  return (
+    <header className={headerClass}>
+      <Link to="/">
+        <img className="header__img" src={logo} alt="Platzi Video" />
+      </Link>
+      <div><h2>Libreria de Juancho</h2></div>
+      <div className="header__menu">
+        <div className="header__menu--profile">
+
+          {hasUser ?
+            <img src={gravatar(user.email)} alt={user.email} /> :
+            <img src={userIcon} alt="" />
+          }
+          <p>Perfil</p>
+        </div>
+        <ul>
+
+          {hasUser ?
+            <li>
+              <Link to="/">{user.name}</Link>
+            </li> : null
+          }{
+            hasUser ?
+              <li>
+                <Link to="#logout" onClick={handleLogout}>Cerrar Sesión</Link>
+              </li> :
+              <Link to="/login">Iniciar sesion</Link>
+          }
+        </ul>
       </div>
-      <ul>
-        <li><a href="/">Cuenta</a></li>
-        <li><a href="/">Cerrar Sesión</a></li>
-      </ul>
-    </div>
-  </header>
-);
+    </header>
+  );
+}
+const mapStateToProps = state => {
+  return {
+    user: state.user
+  };
+}
 
-export default Header;
+const mapDispatchToPros = {
+  logoutRequest,
+}
+
+export default connect(mapStateToProps, mapDispatchToPros)(Header);
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import '../assets/styles/components/Search.scss';
+import className from 'classnames';
 
-const Search = () => (
-  <section className="main">
-    <h2 className="main__title">¿Qué quieres ver hoy?</h2>
-    <input type="text" className="input" placeholder="Buscar..." />
-  </section>
-);
+const Search = ({ isHome }) => {
+  const inputStyle = className('input', {
+    isHome
+  });
+  return (
+    <section className="main">
+      <h2 className="main__title">¿Qué quieres leer hoy?</h2>
+
+    </section>
+  );
+}
 
 export default Search;
